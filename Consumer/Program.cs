2b2a3a797c1baa299907030c39e567637fa57e42
Ata Akalın <ata.akalinn99@outlook.com>
2022-09-19 02:30:06 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Consumer---");
+            Consumer consumer = new Consumer();
+            consumer.GetMessageFromRabbitMq();
+        }
+    }
+    class Consumer
+    {
+        public void GetMessageFromRabbitMq()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    Thread.Sleep(1000);
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received [{0}]", message);
+                };
+
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
---
+++ Publisher/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Publisher---");
+            Console.WriteLine("Please enter your message");
+
+
+            Publisher publisher = new Publisher();
+            string sentMessage = string.Empty;
+            while (!sentMessage.Contains("q"))
+            {
+                sentMessage = Console.ReadLine();
+                publisher.SendToRabbitMQ(sentMessage);
+            }
+
+        }
+    }
+
+
+    class Publisher
+    {
+        public void SendToRabbitMQ(string paramMessage)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                ///<summary>
+                ///Queue: “hello” bu parametre ile göndermek istediğimiz Queue adını belirtilir.
+                ///Durable: Bu parametre ile yeniden başlatma sonrasında verilerin kuyrukta kalıp kalmayacağı belirtilir.
+                /// Exclusive: Kuyruğa birden fazla bağlantı açılıp açılamayacağını belirtir.
+                /// Auto Delete: Son tüketici(Consumer) silindiğinde, kuyruğun silinip silinmeyeceğini belirtir.
+                ///</summary>
+                channel.QueueDeclare(queue: "hello",
+                    durable: true,      
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+
+                var body = Encoding.UTF8.GetBytes(paramMessage);
+                channel.BasicPublish(exchange: "",
+                    routingKey: "hello",
+                    body: body);
+                Console.WriteLine("Message [{0}] sent", paramMessage);
+            }
+        }
+    }
+}
